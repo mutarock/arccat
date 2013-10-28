f6759a821c5729d3c3e50e20b2b21f177f7fc009
wookay <wookay.noh@gmail.com>
2013-10-28 23:02:21 +0900

    UnitTest run:

--- arccat/UI/UnitTestUI.m
+++ arccat/UI/UnitTestUI.m
@@ -8,24 +8,33 @@
 
 #import "UnitTestUI.h"
 
+UIView* findViewInRootViewController(id rootViewController) {
+    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+        return [[[rootViewController viewControllers] firstObject] view];
+    } else {
+        return [rootViewController view];
+    }
+}
+
 @implementation UnitTest (UI)
 
 +(void) run:(id)ui {
     [self run];
     if (UnitTestManager.sharedInstance.assertions > 0) {
         UIView* view = nil;
-        if ([ui conformsToProtocol:@protocol(UIApplicationDelegate)] || [ui isKindOfClass:[UIApplication class]] || [ui isKindOfClass:[UIWindow class]]) {
-            UIViewController* vc = [[[UIApplication sharedApplication].windows objectAtIndex:0] rootViewController];
-            view = vc.view;
-        } else if ([ui isKindOfClass:[UIView class]]) {
+        if ([ui conformsToProtocol:@protocol(UIApplicationDelegate)]) {
+            view = findViewInRootViewController([[ui window] rootViewController]);
+        } else if ([ui isKindOfClass:[UIApplication class]]) {
+            view = findViewInRootViewController([[[ui windows] firstObject] rootViewController]);
+        } else if ([ui isKindOfClass:[UIView class]] || [ui isKindOfClass:[UIWindow class]]) {
             view = ui;
         } else if ([ui isKindOfClass:[UIViewController class]]) {
             view = ((UIViewController*)ui).view;
         }
         if (nil != view) {
-                view.backgroundColor =
-                    (0 == UnitTestManager.sharedInstance.failures) ?
-                        [UIColor colorWithRed:0.13 green:0.63 blue:0.13 alpha:1] : [UIColor redColor];
+            view.backgroundColor =
+            (0 == UnitTestManager.sharedInstance.failures) ?
+            [UIColor colorWithRed:0.13 green:0.63 blue:0.13 alpha:1] : [UIColor redColor];
         }
     }
 }
